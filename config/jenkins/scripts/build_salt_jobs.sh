8338f51f7b728317b6ced60c80ced92bec00876c
Celine <celine>
2026-02-27 12:34:31 +0200

    Jenkins > bash script to automatically create refresh jobs for SaltStack

--- config/jenkins/scripts/build_salt_jobs.sh
+++ config/jenkins/scripts/build_salt_jobs.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
+#===============================================================================
+#         FILE: build_salt_jobs.sh
+#       AUTHOR: Celine H.
+#      COMMENT: The scripts will loop in your salt repository and collect all saltenv
+#               Once the list is fetched it will submit them to Jenkins
+#               One job per Grain with the list of saltenv available for the grain in param
+#               Create a .env file with the following variables [JENKINS_URL=http://host.plop.local:8080 JENKINS_USER=xxx JENKINS_TOKEN=xxx]
+#===============================================================================
+
 # /srv/saltstack
 declare -A SALTBYHOSTS
 source .env
 function sort_format() {
   myxml=""
-  uniqs_arr=($(echo "${SALTBYHOSTS[$i]}" | tr ' ' '\n' | sort -u ))
-  IFS=$'\n'
-  for s in "${uniqs_arr[@]}"
+  mystring=$1
+
+  # Sort array and remove all duplicates
+  uniqs_arr=($(echo "${mystring}" | tr ' ' '\n' | sort -u ))
+  # Replace newline by ,
+  uniqs_arr=${uniqs_arr//[$'\t\r\n']/,}
+
+  IFS="," read -ra uarr <<<"$uniqs_arr"
+  for s in "${uarr[@]}"
   do
-    myxml+="<string>$s</string>"
+    if [[ ! -z $s ]]; then
+      myxml+="<string>$s</string>"
+    fi
   done
 }
 
@@ -62,32 +79,36 @@ do
 
   ((cpt++))
 done
-echo "## Im Done looping"
+echo "## Im Done looping, building the xml job files and submitting them to Jenkins"
 
-# Checking how it looks like
 for i in "${!SALTBYHOSTS[@]}"
 do
   sort_format ${SALTBYHOSTS[$i]}
-  echo "key: ${i} value: >> $myxml"
+  echo "Building template for > key: [${i}] value: >> [$myxml]"
 
-  buildJob=$(sed "s/MYHOSTTOREPLACE/${i}/;s|MYSALTENVTOREPLACE|${myxml}|" ./Refresh.xml)
+  host=${i}
+  if [[ ${i} == "all" ]]; then
+    host="*"
+  fi
+
+  buildJob=$(sed "s/MYHOSTTOREPLACE/${host}/;s|MYSALTENVTOREPLACE|${myxml}|" ./Refresh.xml)
 
   job_name="Refresh_${i%.*.*}"
   echo ${buildJob} > "${job_name}.xml"
 
+  # Fetching crumbs
   CRUMB=$(curl -s "${JENKINS_USER}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)" -u ${JENKINS_USER}:${JENKINS_TOKEN})
 
-  #--silent --head
+  # Checking if the job already exists
   CHECK=$(curl -X GET -s -o /dev/null -w "%{http_code}" ${JENKINS_URL}/view/all/job/${job_name}/config.xml -u ${JENKINS_USER}:${JENKINS_TOKEN})
 
-  #echo "CHECKING $CHECK"
+
   if [[ $CHECK == "200" ]];then
     # This api call will update an existing job
     echo "$CHECK should be okay, we will try to update ${job_name}"
     curl -s -X POST ${JENKINS_URL}/job/${job_name}/config.xml --data-binary @./${job_name}.xml -u ${JENKINS_USER}:${JENKINS_TOKEN} -H "Content-Type:text/xml" -H "$CRUMB" -H "Content-Type:text/xml"
   else
     echo "$CHECK does not seem to be okay, we will try to create ${job_name}"
-
     # This api call will create the job
     curl -s -X POST "${JENKINS_URL}/createItem?name=${job_name}" -u ${JENKINS_USER}:${JENKINS_TOKEN} --data-binary @./${job_name}.xml -H "Content-Type:text/xml" -H "$CRUMB" -H "Content-Type:text/xml"
   fi
